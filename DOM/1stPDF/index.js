7d3f86fa949f4a5bc82d365ad4461ad08e73be85
Malik Ishtaiwe <malekishtiwi3@gmail.com>
2025-02-05 00:55:07 +0300

    finished the crud table and the notepad dom tasks

---
+++ DOM/1stPDF/index.js
@@ -0,0 +1,7 @@
+var uname = document.getElementById("uname");
+
+var uLabel = document.createElement("label");
+
+var labelText = document.createTextNode("Name: ");
+
+uLabel.appendChild(labelText);
